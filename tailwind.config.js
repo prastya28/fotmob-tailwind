c7d4f9228d66dc63b1049feea24293fc1b802ef1
kizznaiver <ki2naiver@protonmail.com>
2024-02-25 04:15:42 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins'],
+      },
+      fontSize: {
+      },
+      backgroundImage: {},
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
